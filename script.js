0e398a934f57ebd5ec2e2cb4c0849ed2300c370d
Minov Vitaliy <minov.vitaliy@tirolhelpsukraine.eu>
2023-08-01 09:32:22 +0200

    using if and else if

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-const buttons = document.querySelectorAll("button")
+const myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
 
-buttons.forEach((button) => {
-  button.addEventListener("click", () => {
-    const currentClicks = parseInt(button.dataset.clicks)
-
-    button.dataset.clicks = currentClicks + 1
-  })
-})
+if (myArray.length === 0) {
+  console.log("empty")
+} else if (myArray.length < 5) {
+  console.log("small")
+} else if (myArray.length < 10) {
+  console.log("medium")
+} else {
+  console.log("large")
+}
